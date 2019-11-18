c426c853e43e247d308379cc47d8bfe2988f5180
Steve Persch <stevector@users.noreply.github.com>
2019-11-18 10:44:14 -0600

    Update index.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -9,8 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site. On Pantheon. With CircleCI
-    <br>testing a Pull Request with CircleCI</p>
+    <p>Welcome to your new Gatsby site. On Pantheon. With CircleCI. And Testing a Pull Request with CircleCI</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
